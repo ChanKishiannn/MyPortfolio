170cab6c7a0118cfb78cf9aaf0b29669ed35af42
Christian M. Abuda <chankishiannn@gmail.com>
2025-09-05 21:32:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger-icon");
 const navbarButtons = document.querySelectorAll(".navbar-button");
 
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navbarButtons.forEach(button => {
